bc6ac92708143181ec1097c55b07d9f897f9e814
sameerasw <sameerasw.com@gmail.com>
2026-09-24 01:03:04 +0530

    feat: replace searchbar lottie

--- app/src/main/java/com/sameerasw/essentials/domain/registry/FeatureRegistry.kt
+++ app/src/main/java/com/sameerasw/essentials/domain/registry/FeatureRegistry.kt
@@ -595,6 +595,7 @@ object FeatureRegistry {
                 hasMoreSettings = true,
                 isBeta = true,
                 parentFeatureId = "Widgets",
+                animationRes = R.raw.searchbar_motion,
             ) {
                 override fun isEnabled(viewModel: MainViewModel) = viewModel.isPixelSearchbarEnabled.value
 
